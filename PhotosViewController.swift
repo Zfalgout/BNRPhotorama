3b1a26b788c07cbd6963b0aac376e8d3e8dad86e
Zack Falgout <zfalgout@gmail.com>
2017-05-18 06:19:25 -0500

    Started chapter 20

---
+++ PhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Zack Falgout on 5/18/17.
+//  Copyright © 2017 Zack Falgout. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController{
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    
+}
