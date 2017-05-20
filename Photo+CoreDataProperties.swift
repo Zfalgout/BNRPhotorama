690284e35bbaa7008a73026c89a0638544c08cdf
Zack Falgout <zfalgout@gmail.com>
2017-05-20 16:15:47 -0500

    Finished chapter 23

--- Photo+CoreDataProperties.swift
+++ Photo+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Photo+CoreDataProperties.swift
 //  Photorama
 //
-//  Created by Zack Falgout on 5/19/17.
+//  Created by Zack Falgout on 5/20/17.
 //  Copyright © 2017 Zack Falgout. All rights reserved.
 //
 
@@ -16,9 +16,27 @@ extension Photo {
         return NSFetchRequest<Photo>(entityName: "Photo")
     }
 
-    @NSManaged public var photoID: String?
-    @NSManaged public var title: String?
     @NSManaged public var dateTaken: NSDate?
+    @NSManaged public var photoID: String?
     @NSManaged public var remoteURL: NSURL?
+    @NSManaged public var title: String?
+    @NSManaged public var tag: NSSet?
+
+}
+
+// MARK: Generated accessors for tag
+extension Photo {
+
+    @objc(addTagObject:)
+    @NSManaged public func addToTag(_ value: Tag)
+
+    @objc(removeTagObject:)
+    @NSManaged public func removeFromTag(_ value: Tag)
+
+    @objc(addTag:)
+    @NSManaged public func addToTag(_ values: NSSet)
+
+    @objc(removeTag:)
+    @NSManaged public func removeFromTag(_ values: NSSet)
 
 }
